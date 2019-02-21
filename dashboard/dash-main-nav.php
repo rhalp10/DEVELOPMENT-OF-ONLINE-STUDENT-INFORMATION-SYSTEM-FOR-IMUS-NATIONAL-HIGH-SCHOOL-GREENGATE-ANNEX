c20fb3837536d96a267b5312ef01e831607a06c4
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-02-22 00:51:09 +0800

    dash-main-nav

--- dashboard/dash-main-nav.php
+++ dashboard/dash-main-nav.php
@@ -5,20 +5,12 @@
 
 								<!-- Main -->
 								<li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
-								<li><a href="index.html"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
-								<li>
-									<a href="#"><i class="icon-stack2"></i> <span>Page layouts</span></a>
-									<ul>
-										<li><a href="layout_navbar_fixed.html">Fixed navbar</a></li>
-										<li class="navigation-divider"></li>
-										<li><a href="boxed_default.html">Boxed with default sidebar</a></li>
-									</ul>
-								</li>
+								<li><a href="index"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
 								<!-- /main -->
-								<!-- RECORD -->
-								<li class="navigation-header"><span>RECORD</span> <i class="icon-menu" title="Forms"></i></li>
+
+								<!-- ADMISSION -->
 								<li>
-									<a href="#"><i class="icon-pencil3"></i> <span>report 1</span></a>
+									<a href="#"><i class="icon-person"></i> <span>ADMISSION</span></a>
 									<ul>
 										<li><a href="form_inputs_basic.html">Basic inputs</a></li>
 										<li>
@@ -33,11 +25,58 @@
 									</ul>
 								</li>
 								
-								<!-- /RECORD -->
-								<!-- REPORT -->
-								<li class="navigation-header"><span>REPORT</span> <i class="icon-menu" title="Forms"></i></li>
+								<!-- /ADMISSION -->
+								<!-- RECORD -->
 								<li>
-									<a href="#"><i class="icon-pencil3"></i> <span>report 1</span></a>
+									<a href="#"><i class="icon-info22"></i> <span>INFORMATION RECORD</span></a>
+									<ul>
+										<li>
+											<a href="#">STUDENT RECORDS</a>
+											<ul>
+												<li><a href="dash-content-student">Student list</a></li>
+												<li><a href="dash-content-student">Top student</a></li>
+												<li><a href="dash-content-student">Alumni student</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">PARENT RECORDS</a>
+											<ul>
+												<li><a href="dash-content-student">Parent list</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">EMPLOYEE RECORDS</a>
+											<ul>
+												<li><a href="dash-content-student">Employee list</a></li>
+												<li><a href="dash-content-student">Organization Chart</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								
+								<!-- /RECORD -->
+								<!-- GRADE -->
+								<li>
+									<a href="#"><i class="icon-pencil3"></i> <span>GRADE RECORD</span></a>
+									<ul>
+										<li><a href="form_inputs_basic.html">Basic inputs</a></li>
+										<li>
+											<a href="#">Selects</a>
+											<ul>
+												<li><a href="form_select2.html">Select2 selects</a></li>
+												<li><a href="form_multiselect.html">Bootstrap multiselect</a></li>
+												<li><a href="form_select_box_it.html">SelectBoxIt selects</a></li>
+												<li><a href="form_bootstrap_select.html">Bootstrap selects</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								
+								<!-- /GRADE -->
+								<!-- REPORT -->
+								<li class="navigation-header"><span>REPORT MODULE</span> <i class="icon-menu" title="Forms"></i></li>
+								<li>
+									<a href="#"><i class="icon-newspaper2"></i> <span>REPORT RECORD</span></a>
 									<ul>
 										<li><a href="form_inputs_basic.html">Basic inputs</a></li>
 										<li>
