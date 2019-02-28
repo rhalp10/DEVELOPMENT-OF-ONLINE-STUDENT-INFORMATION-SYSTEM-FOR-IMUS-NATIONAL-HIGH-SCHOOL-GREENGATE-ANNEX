736c4ae334f7682e40582b6167436687ad325ab5
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-01 03:52:48 +0800

    session

--- session.php
+++ session.php
@@ -5,18 +5,19 @@ session_start(); // Starting Session
 // Storing Session
 $user_check = $_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-	$ses_sql = mysqli_query($con,"SELECT user_Name,user_level,ulevel_ID FROM user_accounts WHERE user_Name='$user_check'");
+	$ses_sql = mysqli_query($con,"SELECT user_Name,user_ID,ulevel_ID FROM user_accounts WHERE user_Name='$user_check'");
 	$row = mysqli_fetch_assoc($ses_sql);
 	$login_session = $row['user_Name'];
-	$login_level = $row['user_level'];
-	$login_id = $row['ulevel_ID'];
-	$_SESSION['login_id'] = $row['ulevel_ID'];
+
+	$_SESSION['login_level'] = $row['ulevel_ID'];
+	$_SESSION['login_id'] = $row['user_ID'];
+	$_SESSION['user_Name'] = $row['user_Name'];
 
 
 if (!isset($login_session))
 {
   mysqli_close($con); // Closing Connection
-  header('Location: index.php'); // Redirecting To Home Page
+  header('Location: ../index.php'); // Redirecting To Home Page
 }
 
 ?>
