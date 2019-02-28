40b7f09d7340908b9eea73a1fd98c37b50903be9
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-01 03:52:37 +0800

    admin register

--- register.php
+++ register.php
@@ -6,3 +6,13 @@ email: <input type="email" name="email"><br>
 
 <input type="submit" name="register-student" value="submit">
 </form>
+
+
+<form action="data-register.php" method="post">
+username: <input type="text" name="username"><br>
+password: <input type="text" name="password"><br>
+cpassword: <input type="text" name="re_password"><br>
+email: <input type="email" name="email"><br>
+
+<input type="submit" name="register-admin" value="submit">
+</form>
