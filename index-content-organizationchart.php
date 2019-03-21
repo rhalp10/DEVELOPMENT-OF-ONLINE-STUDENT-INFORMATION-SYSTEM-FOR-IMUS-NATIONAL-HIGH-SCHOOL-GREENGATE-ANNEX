5435b0e2b6bab096cd65235865af235d68c84b00
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-21 17:07:21 +0800

    index organizationchart

--- index-content-organizationchart.php
+++ index-content-organizationchart.php
@@ -1,5 +1,5 @@
 <h1 itemprop="name"><strong>Organizational Chart</strong></h1>
-
 <hr />
-
+<center>
 <img src="assets/images/school-organizational-chart-edited-1_orig.png" alt="Picture" style="width:auto;max-width:100%">
+</center>
