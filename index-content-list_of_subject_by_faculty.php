925319b1b777d1e31ee3904af099f103b1507c05
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-21 17:07:52 +0800

    staff

--- index-content-list_of_subject_by_faculty.php
+++ index-content-list_of_subject_by_faculty.php
@@ -1,41 +1,61 @@
-<h1 itemprop="name"><strong>index-content-list_of_subject_by_faculty.php
-</strong></h1>
-
-<hr />
-<div class="panel panel-flat">
-	<div class="panel-heading">
-		<h6 class="panel-title"></h6>
-	</div>
-	
-	<div class="panel-body">
-		<table class="table">
-			<thead>
-				<th>
-					Name
-				</th>
-				<th>
-					Position
-				</th>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Rhalp Darren Cabrera</td>
-					<td>ADMIN</td>
-				</tr>
-				<tr>
-					<td>Rhalp Darren Cabrera</td>
-					<td>ADMIN</td>
-				</tr>
-				<tr>
-					<td>Rhalp Darren Cabrera</td>
-					<td>ADMIN</td>
-				</tr>
-				<tr>
-					<td>Rhalp Darren Cabrera</td>
-					<td>ADMIN</td>
-				</tr>
-			</tbody>
-
-		</table>
-	</div>
+<?php 
+	include('dbconfig.php');
+?>
+<div class="bg-blue" style="width: auto; padding: 20px;">
+<h2>Academic Staff</h2>
+<hr>
 </div>
+<br>
+<?php 
+			// SQL query to fetch information of registerd users and finds user match.
+			$query = mysqli_query($con,"SELECT * FROM `subject`");
+			if (mysqli_num_rows($query) > 0) 
+			{
+				// And error has occured while executing
+			   while ($rows = mysqli_fetch_assoc($query)) {
+			   	 $subject_TItle = $rows['subject_TItle'];		   	
+			   	?>
+					<div class="col-sm-4">
+						<div class="panel panel-default">
+						  <div class="panel-heading"><?php echo $subject_TItle?> </div>
+						  <div class="panel-body" style="padding: 0px;">
+						  	<table class="table table-bordered">
+						  		<thead>
+						  		<th>Name</th>
+						  		<th>Position</th>
+						  		</thead>
+						  		<tbody>
+						  			<tr>
+						  				<td>NAME NAME NAME</td>
+						  				<td>Position</td>
+						  			</tr>
+						  			<tr>
+						  				<td>NAME NAME NAME</td>
+						  				<td>Position</td>
+						  			</tr>
+						  			<tr>
+						  				<td>NAME NAME NAME</td>
+						  				<td>Position</td>
+						  			</tr>
+						  			<tr>
+						  				<td>NAME NAME NAME</td>
+						  				<td>Position</td>
+						  			</tr>
+						  		</tbody>
+						  	</table>
+						</div>
+						<div class="panel-footer" style="padding: 10px;">
+							
+						</div>
+						</div>
+
+					</div>
+					<?php
+			   }
+				
+			
+			}
+
+
+
+?>
