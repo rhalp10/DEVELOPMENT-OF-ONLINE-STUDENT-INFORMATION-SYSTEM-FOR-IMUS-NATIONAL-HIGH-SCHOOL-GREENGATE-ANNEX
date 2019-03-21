7aa0ea27e0ddda3aaf10c08c5604417ac52cb19f
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-21 17:07:27 +0800

    index content

--- index-content.php
+++ index-content.php
@@ -1,83 +1,61 @@
+<div class="row text-center" style="margin-top: 450px;">
+  <div class="col-sm-6">
+    <div  style="margin-bottom: -150px;">
+      
+    <img src="assets/images/logo.png" class="" width="40%" >
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
-<div class=" container bg-slate " style="width: 100vw;
- position: relative;
- margin-left: -50vw;
- left: 50%;
- padding: 50px;">
-      <div class="wsite-section-elements">
-        <div><div class="wsite-multicol"><div class="wsite-multicol-table-wrap" style="margin:0 -25px;">
-  <table class="wsite-multicol-table">
-    <tbody class="wsite-multicol-tbody">
-      <tr class="wsite-multicol-tr">
-        <td class="wsite-multicol-col" style="width:44.989106753812%; padding:0 25px;">
-          
-            
-
-<div><div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0;margin-right:0;text-align:center">
-<a>
-<img src="assets/images/principal_2_orig.jpg" alt="Picture" style="width:auto;max-width:100%">
-</a>
-<div style="display:block;font-size:90%"></div>
-</div></div>
-
-<h2 class="wsite-content-title" style="text-align:center;"><font color="#ffffff"><strong><font size="6">The School Head</font></strong></font></h2>
-
-<div><div style="height: 0px; overflow: hidden; width: 50%;"></div>
-<hr class="styled-hr" style="width:50%;">
-<div style="height: 0px; overflow: hidden; width: 50%;"></div></div>
-
-<div class="paragraph" style="text-align:center;"><br><font color="#ffffff">&#8203;</font><font color="#fbf2f2">In our five years existing as molder of the youth… So many challenges were encountered such as shortage of classrooms, chairs, insufficient instructional materials and laboratories but because of the hand in hand working together, we recuperated easily.&nbsp;<br>For all my colleagues I salute to your fashion in your chosen career. Your patience and motherly approach to our students is highly appreciated. I don’t heard any negative comment from you even experienced teaching under the tent handling 125 students per class. Your hard work gave you an amazing results. You are not only a teacher who impart knowledge to our students but you are like a candle to them who give light in their life and shaping their humanity’s future.&nbsp;<br>To our dear students, we are helping you to face the reality of life but you are the one who will help to yourself in pursuing your goal. Listen to us when we tell you WHAT to do. But more importantly, think about it and ask yourself WHY we told you to do it. Do not let others to ruin your life you are the driver of your own life.&nbsp; Life is all about CHANCES and OPPORTUNITIES. Never let your mind leave anything to CHANCE and never let an OPPORTUNITY get away. The opportunity given your parents – supporting you in your studies grab it do not let it away. It is the first step in doing a difference in your life. Focus on your studies.&nbsp; And when you are there always put your feet on the ground. Be an educated person with a good character.<br><br>In the end, be a little wise!<br>Stay blessed in life!<br><br>Aida C. Bellon<br>Officer-in-Charge</font></div>
-
-
-          
-        </td>       <td class="wsite-multicol-col" style="width:55.010893246186%; padding:0 25px;">
-          
-            
-
-<h2 class="wsite-content-title" style="text-align:center;"><strong><font color="#e0bf5c">WHO WE ARE</font></strong></h2>
-
-<div class="paragraph" style="text-align:center;"><font color="#ffffff"><br>&#8203;"<font size="5" style="">W</font>hen we label something as a process, we also mean that it does not have a beginning, an ending, or fixed sequence of events. It is not static. It is moving. The ingredients with a process interact; each affects all of the others." -David Berlo&nbsp;<br>To be "School of Excellence" is the paramount goal of Imus National High School-Greengate Annex. This institution aims for what is best for its stakeholders especially the learners who are the hopes and dreams of this nation. In order to excel, one has to undergo a series of process; a process which has no beginning, middle nor ending. As the proverb says, "Many hands light work." and if each individual knows how to make things work, the process won't be painful, but fruitful. In order to achieve the educational goals set by the Department of Education, Imus National High School-Greengate Annex, together with a pool of competent teachers from each department, has set its plans and objectives to be accomplished by the end of each quarter and at the end of the school year. The school has committed itself in its mission of equipping every learner with competencies and life-long skills capable of surviving the 21st century demands and standards.<br><br>by: Ms. Darizzeth Gelle</font></div>
-
-<div class="wsite-spacer" style="height:50px;"></div>
-
-<h2 class="wsite-content-title" style="text-align:center;"><strong><font color="#e0bf5c" size="7">VISION . MISSION</font></strong></h2>
-
-<div><div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0;margin-right:0;text-align:center">
-<a>
-<img src="assets/images/our-vision-icon.png?1516761813" alt="Picture" style="width:105;max-width:100%">
-</a>
-<div style="display:block;font-size:90%"></div>
-</div></div>
-
-<div class="paragraph" style="text-align:center;"><strong><font color="#f9f5f4"><font size="7">I</font>mus <font size="7">N</font>ational <font size="7">H</font>igh <font size="7">S</font>chool - <font size="7">G</font>reengate <font size="7">A</font></font></strong><font color="#f9f5f4"><strong>nnex </strong><br><strong>envisions its completers as individuals who transform holistically with integrity, ready for global competitiveness and has strong personality in facing the reality of life.</strong></font></div>
-
-<div><div style="height: 20px; overflow: hidden; width: 100%;"></div>
-<hr class="styled-hr" style="width:100%;">
-<div style="height: 20px; overflow: hidden; width: 100%;"></div></div>
-
-<div><div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0;margin-right:0;text-align:center">
-<a>
-<img src="assets/images//mission-icon_orig.png" alt="Picture" style="width:auto;max-width:25%">
-</a>
-<div style="display:block;font-size:90%"></div>
-</div></div>
-
-<div class="paragraph" style="text-align:center;"><font color="#ffffff"><strong style=""><font style=""><font size="7" style="">I</font>mus&nbsp;<font size="7" style="">N</font>ational&nbsp;<font size="7" style="">H</font>igh&nbsp;<font size="7" style="">S</font>chool -&nbsp;<font size="7" style="">G</font>reengate&nbsp;<font size="7" style="">A</font></font></strong><strong>nnex&nbsp;</strong>&#8203;<strong> commits itself to enhance each student’s intellect, promote safe, motivating and supportive environment, strengthen moral and spiritual values, prepare them to act on their belief and accept challenges in life.</strong></font></div>
-
-
-          
-        </td>     </tr>
-    </tbody>
-  </table>
-</div></div></div>
-      </div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" style=" width:100%; height: 500px !important;">
+    <div class="item active">
+       <iframe width="1024" height="606" src="https://www.youtube.com/embed/NfasYVlyYLE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="item">
+      <img src="assets/images/1-2introduction_orig.jpg" alt="Chicago">
     </div>
 
-<h2><strong>What is K to 12 Program?</strong></h2>
+    <div class="item">
+      <img src="assets/images/coronation_orig.jpg" alt="Chicago">
+    </div>
+
+     <div class="item">
+      <img src="assets/images/31655523-2140397729317696-7668192966275497984-n_orig.jpg" alt="Chicago">
+    </div>
+     <div class="item">
+      <img src="assets/images/41295369-571824569918608-1817510269950099456-o_orig.jpg" alt="Chicago">
+    </div>  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  </div>
+</div>    <br>
 
 
+<div style="padding-top: 60em;"></div>
 
-<div class="embed-responsive embed-responsive-16by9">
+
+<div class="panel panel-default" >
+  <div class="panel-heading"></div>
+  <div class="panel-body">
+    <h2><strong>What is K to 12 Program?</strong></h2>
+    <div class="embed-responsive embed-responsive-16by9">
    <iframe width="1024" height="606" src="https://www.youtube.com/embed/NfasYVlyYLE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 <div class="paragraph" style="text-align:center;">K to 12 (also K-12) is an education system under the Department of Education that aims to enhance learners’ basic skills, produce more competent citizens, and prepare graduates for lifelong learning and employment.&nbsp;<span style="color:rgb(185, 37, 85)">“K”</span>&nbsp;stands for Kindergarten and&nbsp;<span style="color:rgb(185, 37, 85)">“12”</span>&nbsp;refers to the succeeding 12 years of basic education (6 years of elementary education, 4 years of junior high school, and 2 years of senior high school).<br><span></span>At present, the Philippines is the last country in Asia and one of only three countries in the world with a 10-year pre-university program.<br><span></span></div>
@@ -109,6 +87,24 @@
           <td class="cell" style="width: 49%;"><div class="paragraph"><ul style="color:rgb(40, 50, 63)"><li>Old education system offers a broad curriculum that does not include enough practical applications.</li></ul>&#8203;&#8203;</div></td>
       </tr>
   </tbody></table>
+</div>
+</div>
+
+<!-- Basic markers -->
+<!-- <div class="panel panel-flat" style="margin-bottom: 0px;">
+  <div class="panel-heading">
+   
+  </div>
+
+  <div class="panel-body">
+    <div id="googleMap" style="width:100%;height:400px;"></div>
+    
+  </div>
+</div> -->
+
+
+
+
 
 
   
