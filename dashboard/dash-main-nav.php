3e5a2f913d6cf33434731af9ee6bc2baaa3fa48b
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-28 05:06:02 +0800

    dash-main-nav

--- dashboard/dash-main-nav.php
+++ dashboard/dash-main-nav.php
@@ -1,3 +1,153 @@
+<?php 
+function side_admission(){
+?>
+<!-- ADMISSION -->
+<li>
+	<a href="#"><i class="icon-person"></i> <span>ADMISSION</span></a>
+	<ul>
+		<li><a href="index?page=admission">Admission List</a></li>
+	</ul>
+</li>
+<!-- /ADMISSION -->
+<?php
+}
+function side_account(){
+?>
+<!-- ADMISSION -->
+<li>
+	<a href="#"><i class="icon-person"></i> <span>ACCOUNT</span></a>
+	<ul>
+		<li><a href="index?page=account">Account List</a></li>
+	</ul>
+</li>
+<!-- /ADMISSION -->
+<?php
+}
+function side_section(){
+?>
+<!-- ADMISSION -->
+<li>
+	<a href="#"><i class="icon-person"></i> <span>SECTION</span></a>
+	<ul>
+		<li><a href="index?page=section">Section List</a></li>
+		<li><a href="index?page=section_assign">Section List</a></li>
+	</ul>
+</li>
+<!-- /ADMISSION -->
+<?php
+}
+function side_student_section(){
+	?>
+<!-- ADMISSION -->
+<li>
+	<a href="#"><i class="icon-book"></i> <span>INFORMATIONS</span></a>
+	<ul>
+		<li><a href="#" data-toggle="modal" data-target="#enrolled_subject">Enrolled Subject</a></li>
+		<li><a href="#" data-toggle="modal" data-target="#enrolled_subject_grade">Latest Grade</a></li>
+	</ul>
+</li>
+<!-- /ADMISSION -->
+<?php
+}
+function side_parent_section(){
+	?>
+<!-- ADMISSION -->
+<li>
+	<a href="#"><i class="icon-book"></i> <span>INFORMATIONS</span></a>
+	<ul>
+		<li><a href="#" data-toggle="modal" data-target="#child_record">Child Record</a></li>
+	</ul>
+</li>
+<!-- /ADMISSION -->
+<?php
+}
+function side_record(){
+?>
+<!-- RECORD -->
+<li>
+	<a href="#"><i class="icon-info22"></i> <span>INFORMATION RECORD</span></a>
+	<ul>
+		<li>
+			<a href="index?page=student-list">STUDENT RECORDS</a>
+		</li>
+		<li>
+			<a href="index?page=parent-list">PARENT RECORDS</a>
+		</li>
+		<li>
+			<a href="index?page=employee-list">TEACHER RECORDS</a>
+			
+		</li>
+		<li>
+			<a href="index?page=subject-list">SUBJECT RECORDS</a>
+		</li>
+		<li>
+			<a href="index?page=yearlevel-list">YEAR LEVEL</a>
+		</li>
+		<li>
+			<a href="index?page=teacher-with-subject-assign">TEACHER SUBJECT</a>
+		</li>
+		<li>
+			<a href="index?page=section-list">SECTION RECORD</a>
+		</li>
+		<li>
+			<a href="index?page=semester-list">SEMESTER RECORD</a>
+		</li>
+	</ul>
+</li>
+<!-- /RECORD -->
+<?php
+}
+
+function side_grade(){
+?>
+<!-- GRADE -->
+<li><a href="index?page=grade"><i class="icon-pencil3"></i> <span>GRADE PER SECTION</span></a></li>
+<!-- /GRADE -->
+<?php
+}
+
+function side_parent(){
+?>
+<!-- PARENT -->
+<li>
+	<a href="#"><i class="icon-person"></i> <span>CHILD RECORD</span></a>
+	<ul>
+		<li>
+			<a href="index?page=grade&of_student=1">ANAK 1</a>
+			
+		</li>
+		<li>
+			<a href="index?page=grade&of_student=1">ANAK 2</a>
+			
+		</li>
+	</ul>
+</li>
+<!-- /PARENT -->
+<?php
+}
+function side_report(){
+?>
+<!-- REPORT -->
+<li class="navigation-header"><span>REPORT MODULE</span> <i class="icon-menu" title="Forms"></i></li>
+<li>
+	<a href="#"><i class="icon-newspaper2"></i> <span>REPORT RECORD</span></a>
+	<ul>
+		<li><a href="form_inputs_basic.html">Basic inputs</a></li>
+		<li>
+			<a href="#">Selects</a>
+			<ul>
+				<li><a href="form_select2.html">Select2 selects</a></li>
+				<li><a href="form_multiselect.html">Bootstrap multiselect</a></li>
+				<li><a href="form_select_box_it.html">SelectBoxIt selects</a></li>
+				<li><a href="form_bootstrap_select.html">Bootstrap selects</a></li>
+			</ul>
+		</li>
+	</ul>
+</li>
+<!-- /REPORT -->
+<?php
+}
+?>
 <!-- Main navigation -->
 					<div class="sidebar-category sidebar-category-visible">
 						<div class="category-content no-padding">
@@ -7,109 +157,37 @@
 								<li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
 								<li><a href="index"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
 								<!-- /main -->
-								<?php if ($login_level == 4): ?>
-								<!-- ADMISSION -->
-								<li>
-									<a href="dash-content-admission"><i class="icon-person"></i> <span>ADMISSION</span></a>
-									<ul>
-										<li><a href="index?page=admission">Admission List</a></li>
-									</ul>
-								</li>
-								<!-- /ADMISSION -->
-								<?php else: ?>
-									
-								<?php endif ?>
-								<?php if ($login_level == 4): ?>
-								<!-- RECORD -->
-								<li>
-									<a href="#"><i class="icon-info22"></i> <span>INFORMATION RECORD</span></a>
-									<ul>
-										<li>
-											<a href="index?page=student">STUDENT RECORDS</a>
-											<ul>
-												<li><a href="index?page=student-list">Student list</a></li>
-												<li><a href="index?page=student-top">Top student</a></li>
-												<!-- <li><a href="index?page=student-alumni">Alumni student</a></li> -->
-											</ul>
-										</li>
-										<?php if ($login_level == 1): ?>
-										<li>
-											<a href="#">PARENT RECORDS</a>
-											<ul>
-												<li><a href="index?page=parent">Parent list</a></li>
-											</ul>
-										</li>
-										<li>
-											<a href="#">TEACHER RECORDS</a>
-											<ul>
-												<li><a href="index?page=employee">Teacher list</a></li>
-												<!-- <li><a href="index?page=employee">Organization Chart</a></li> -->
-											</ul>
-										</li>
-										<?php else: ?>
-									
-										<?php endif ?>	
-									</ul>
-								</li>
-								
-								<!-- /RECORD -->
-								<?php else: ?>
-									
-								<?php endif ?>
-								<?php if ($login_level == 1): ?>
-								<!-- GRADE -->
-								<li><a href="index?page=grade"><i class="icon-pencil3"></i> <span>GRADE PER SECTION</span></a></li>
-								<!-- /GRADE -->
-								<?php else: ?>
-									
-								<?php endif ?>
-								<?php if ($login_level == 3): ?>
-								<!-- PARENT -->
-								<li>
-									<a href="#"><i class="icon-person"></i> <span>CHILD RECORD</span></a>
-									<ul>
-										<li>
-											<a href="index?page=grade&of_student=1">ANAK 1</a>
-											
-										</li>
-										<li>
-											<a href="index?page=grade&of_student=1">ANAK 2</a>
-											
-										</li>
-									</ul>
-								</li>
-								<!-- /PARENT -->
-								<?php else: ?>
-									
-								<?php endif ?>
-								<?php if ($login_level == 4): ?>
-								<!-- REPORT -->
-								<li class="navigation-header"><span>REPORT MODULE</span> <i class="icon-menu" title="Forms"></i></li>
-								<li>
-									<a href="#"><i class="icon-newspaper2"></i> <span>REPORT RECORD</span></a>
-									<ul>
-										<li><a href="form_inputs_basic.html">Basic inputs</a></li>
-										<li>
-											<a href="#">Selects</a>
-											<ul>
-												<li><a href="form_select2.html">Select2 selects</a></li>
-												<li><a href="form_multiselect.html">Bootstrap multiselect</a></li>
-												<li><a href="form_select_box_it.html">SelectBoxIt selects</a></li>
-												<li><a href="form_bootstrap_select.html">Bootstrap selects</a></li>
-											</ul>
-										</li>
-									</ul>
-								</li>
-								<!-- /REPORT -->
-								<?php else: ?>
-									
-								<?php endif ?>
-								
+								<?php 
+								//ADMIN
+								if ($login_level == 1) {
+									side_admission();
+									side_account();
+									side_section();
+									// side_record();
+									// side_grade();
+									// side_parent();
+									// side_report();
+								} 
+								//STUDENT
+								else if ($login_level == 2){
+									side_student_section();
 
-								
+								}
+								//PARENT
+								else if ($login_level == 3){
+									side_parent_section();
 
-								
+								}
+								//TEACHER
+								else if ($login_level == 4){
+									side_admission();
 
+								}
+								else {
+									# code...
+								}
+								
+								?>
 							</ul>
 						</div>
 					</div>
