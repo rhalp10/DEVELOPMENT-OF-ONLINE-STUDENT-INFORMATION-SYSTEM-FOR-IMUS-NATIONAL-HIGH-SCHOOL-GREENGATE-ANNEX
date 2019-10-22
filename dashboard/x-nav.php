fc2cc4eeaee70fbc8ef5ee3679c4fc06c874d1b8
Rhalp Darren Cabrera <rhalpdarrencabrera@gmail.com>
2019-10-22 20:19:59 +0800

    Dashboard Nav

--- dashboard/x-nav.php
+++ dashboard/x-nav.php
@@ -2,7 +2,36 @@
  <nav class="navbar  navbar-expand-lg  navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" style="background-color: #4caf50 !important;">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#" style="width:relative; font-size: 0.9rem;">INHS-GA Information System
 </a>
-    
+
+<?php 
+   if($auth_user->admin_level()) { 
+?>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+     
+      <ul class="navbar-nav px-3 ml-auto">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:white;">Notification</a>
+          <ul class="dropdown-menu notify-drop">
+            <div class="notify-drop-title">
+             
+            </div>
+            <!-- end notify title -->
+            <!-- notify content -->
+            <div class="drop-content" style="width: 225px; position:relative;">
+              <div class="d-flex flex-column bd-highlight mb-3" style="overflow: scroll !important; max-height:350px;overflow-x: hidden !important; ">
+                <?php 
+                echo $auth_user->notification();
+                ?>
+              </div>
+            </div>
+            <div class="notify-drop-footer text-center">
+             
+            </div>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  <?php }?>
     <ul class="navbar-nav px-3 ml-auto">
 
 
